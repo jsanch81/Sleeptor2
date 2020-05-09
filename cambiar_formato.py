34a972f5789b7f56a51dac343c41fb240ea5139c
camilo912 <cam.912@hotmail.com>
2020-05-08 21:16:31 -0500

    funcionando con mas videos, con randomForest y hay error en media, se automatiza cambair formato

--- cambiar_formato.py
+++ cambiar_formato.py
@@ -1,7 +1,11 @@
 import subprocess
 import os
 
-path = 'data/05/'
-for fname in os.listdir(path):
-    if(fname.endswith('.MOV') or fname.endswith('.mov')):
-        subprocess.check_call(['ffmpeg', '-i', path+fname, '-vcodec', 'copy', '-acodec', 'copy', path+fname.replace('MOV', 'mp4').replace('mov', 'mp4')])
+folds = [x for x in os.listdir('data/') if x.startswith('Fold')]
+for fold in folds:
+    people = [x for x in os.listdir('data/'+fold) if x.isnumeric()]
+    for person in people:
+        path = 'data/' + fold + '/' + person + '/'
+        for fname in os.listdir(path):
+            if((fname.endswith('.MOV') or fname.endswith('.mov')) and not os.path.isfile(path + fname[:fname.index('.')] + '.mp4')):
+                subprocess.check_call(['ffmpeg', '-i', path+fname, '-vcodec', 'copy', '-acodec', 'copy', path+fname.replace('MOV', 'mp4').replace('mov', 'mp4')])
