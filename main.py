bbc9dfe14f58ba14d6b63c37a1c01fdbab0310fb
admin <admin@PTOF70255.cinfo.eafit.edu.co>
2020-05-12 23:26:16 -0500

    GNRAL: se añade regresión logistica

--- main.py
+++ main.py
@@ -1,24 +1,29 @@
 from feature_extraction import Featurizer
 from model import Model
 from sleeptor import Sleeptor
+import pandas as pd
 def main():
     featurizer = Featurizer()
     data, medias = featurizer.run()
-    # mode = 'concat'
-    mode = 'medias'
 
-    model = Model(featurizer.n_features, mode, featurizer.size, featurizer.n_samples_per_video)
+    df = pd.DataFrame(data)
+    df.to_csv('data.csv')
+    # mode = 'concat'
+    mode = 'concat'
+    type_model = 'logisticRegressi'
+
+    model = Model(featurizer.n_features, mode, featurizer.size, featurizer.n_samples_per_video, type_model)
     if(mode == 'concat'):
-        X = data[:, :-1]
-        y = data[:, [-1]]
-        model.train(X, y)
+       X = data[:, :-1]
+       y = data[:, [-1]]
+       model.train(X, y)
     elif(mode == 'medias'):
         X = medias[:, :-1]
         y = medias[:, [-1]]
         model.train(X, y)
 
-    sleeptor = Sleeptor(featurizer, model)
-    sleeptor.live(mode)
+    # sleeptor = Sleeptor(featurizer, model)
+    # sleeptor.live(mode)
 
 if __name__ == '__main__':
     main()
--- model.py
+++ model.py
@@ -1,4 +1,5 @@
 from sklearn.ensemble import RandomForestClassifier
+from sklearn.linear_model import LogisticRegression
 from sklearn.metrics import accuracy_score
 from sklearn.metrics import confusion_matrix
 from sklearn import metrics
@@ -14,13 +15,15 @@ def normalize(data):
 
 
 class Model():
-    def __init__(self, n_features, mode, size, n_samples):
+    def __init__(self, n_features, mode, size, n_samples, type_model):
         self.n_features = n_features
         self.mode = mode
         self.size = size
-        self.n_samples = n_samples
-
+        self.n_samples = n_samplesx
+        self.type_model = type_model
     def balanced_split(self, X, y, train_size):
+
+        print(self.n_samples)
         X_train = X[:-2*self.n_samples]
         y_train = y[:-2*self.n_samples]
         X_test = X[-2*self.n_samples:]
@@ -29,7 +32,7 @@ class Model():
         return X_train, X_test, y_train, y_test
 
     def train(self, X, y):
-        model_filename = 'data/models/model_' + str(self.n_features) + '_' + str(self.size) + '_' + str(len(X)) + '_' + self.mode + '.pkl'
+        model_filename = 'data/models/model_' + str(self.n_features) + '_' + str(self.size) + '_' + str(len(X)) + '_' + self.type_model + self.mode + '.pkl'
 
         if(os.path.isfile(model_filename)):
             self.model = pickle.load(open(model_filename, 'rb'))
@@ -37,8 +40,13 @@ class Model():
             y = y.ravel()
 
             X_train, X_test, y_train, y_test = self.balanced_split(X, y, 0.7)
+            print('-----------------------------')
+            print(X_train, X_test)
+            if self.type_model == 'logisticRegression':
+                self.model = LogisticRegression(random_state=0)
+            else:
+                self.model = RandomForestClassifier()
 
-            self.model = RandomForestClassifier()
             self.model.fit(X_train, y_train)
             pickle.dump(self.model, open(model_filename, 'wb'))
 
@@ -51,5 +59,3 @@ class Model():
             print(' acc: {}\n f1 score: {}\n roc_auc score: {}'.format(acc, f1, roc))
             print('confusion matrix')
             print(confusion_matrix(y_test, preds))
-
-
